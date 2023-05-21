9766f8dd173aea1ded541862868d35a89dbb2e48
Zsolt Kardos <82752886+O48WRX@users.noreply.github.com>
2023-05-21 21:20:17 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,9 +11,9 @@ module "read" {
 module "write" {
   source = "./modules/write"
   
-  answer_1 = module.write.var.answer_1
-  answer_2 = module.write.var.answer_2
-  answer_3 = module.write.var.answer_3
-  answer_4 = module.write.var.answer_4
-  answer_5 = module.write.var.answer_5
+  answer_1 = module.write.answer_1
+  answer_2 = module.write.answer_2
+  answer_3 = module.write.answer_3
+  answer_4 = module.write.answer_4
+  answer_5 = module.write.answer_5
 }
