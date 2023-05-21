861ce7d05c2c94e66831ec2645d989d576d8e1b1
Zsolt Kardos <82752886+O48WRX@users.noreply.github.com>
2023-05-21 21:49:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,13 @@ module "read" {
 module "write" {
   source = "./modules/write"
 }
+  
+module "data" {
+   source = "./modules/data"
+   file_path = module.files.example_output[0]
+   depends_on = [module.files]
+}  
+
+output "file_id" {
+   value = module.data.file_id
+ }
