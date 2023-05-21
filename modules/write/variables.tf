cfb997624ecf8eb6d723763c27ee7b11e1870f9e
Zsolt Kardos <82752886+O48WRX@users.noreply.github.com>
2023-05-21 20:51:34 +0200

    Update variables.tf

--- modules/write/variables.tf
+++ modules/write/variables.tf
@@ -1,1 +1,19 @@
+variable "answer_1" {
+  description = "Elso valasz"
+}
 
+variable "answer_2" {
+  description = "Masodik valasz"
+}
+
+variable "answer_3" {
+  description = "Harmadik valasz"
+}
+
+variable "answer_4" {
+  description = "Negyedik valasz"
+}
+
+variable "answer_5" {
+  description = "Otodik valasz"
+}
